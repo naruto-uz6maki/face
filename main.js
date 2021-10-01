64d6361b73f333172ba8cf0b5924bf1d75552238
naruto-uz6maki <77130103+naruto-uz6maki@users.noreply.github.com>
2021-10-01 17:43:43 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementsById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementsById("result").innerHTML='<img id="capture_image" src="'+data_uri+'">';
+    });
+}
+console.log('ml5.version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("model loaded");
+}
